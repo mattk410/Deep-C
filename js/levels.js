8063499d342f7473a2faf3f48b99fc7be43139d8
GuanlunMu <guanlunmu@wlan252-076.wireless.stolaf.edu>
2014-12-04 12:44:03 -0600

    Just make everything looks organized

---
+++ js/levels.js
@@ -0,0 +1,27 @@
+var GAME_LEVELS = [
+["                                                                                                      ",
+"                                                                                                       ",
+"                                                                                                       ",
+"                                                                                                       ",
+"                                                                                x                      ",
+"                                                   xx            xxxxx                  o              ",
+"                                    o o      xx                  xxxxx        =                        ",
+"                           =                          =          xxxxx                                 ",
+"                                   xxxxx                       o  xxx                                  ",
+"                                                                                                   xx  ",
+"  xx                                      o o              xx                                       x  ",
+"  x                     o           =                         o                                     x  ",
+"  x                                      xxxxx                     s                  =           o x  ",
+"  x            x     s  o                   s                      s        s                       x  ",
+"  x  @       xxx     s                    s s s                  xxxxx      s     s          xx     x  ",
+"  xxxxxxxxxxxx  xxxxxxxxxxxxxxx           sssss            xxxxxxx   xxxxx  s    ss     o  x   xxxxxx  ",
+"                              x             s              x              xxxxx  s  xxx   x            ",
+"                              x   xxxxxxxxxxxxxxxxxxxx     x                   xxxxx   xxx             ",
+"                              x   x                  x   o x                                           ",
+"                              xxxxx                  xxxxxxx                                           ",
+"                                                                                                       ",
+"                                                                                                       "]
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = GAME_LEVELS;
